d00ec2cbcec4b6f061e417fb1869b9c3e6e6ea66
ifthekar <sajalcse2018@gmail.com>
2023-03-06 14:06:28 +0600

    project complete

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,5 +1,59 @@
 
-document.getElementById('add-btn').addEventListener('click',function(){
-console.log('alert');
+
+
+document.getElementById('add-btn').addEventListener('click',function(e){
+    e.preventDefault();
+
+    const studentName=document.getElementById('stdname');
+    const departMent=document.getElementById('department');
+    const bookName=document.getElementById('bookname');
+    const author=document.getElementById('author');
+    const bookList=document.getElementById('book-list');
+
+    if(studentName.value=='' && departMent.value=='' && bookName.value=='' && author.value==''){
+        alert("please input some values");
+    }
+    else{
+//   creating row
+    newRow=document.createElement('tr');
+
+ //creating new student  
+    const newStudentName=document.createElement('th');
+    newStudentName.innerHTML=studentName.value;
+    newRow.appendChild(newStudentName);
+
+    //creating department name
+    const newDepartmentName=document.createElement('th');
+    newDepartmentName.innerHTML=departMent.value;
+    newRow.appendChild(newDepartmentName);
+    //creating book name
+    const newBookName=document.createElement('th');
+    newBookName.innerHTML=bookName.value;
+    newRow.appendChild(newBookName);
+
+    //creating author name
+    const newAuthorName=document.createElement('th');
+    newAuthorName.innerHTML=author.value;
+    newRow.appendChild(newAuthorName);
+
+    bookList.appendChild(newRow);
+   
+    }
+
+
+  //  refresh self input
+
+  studentName.value="";
+  departMent.value=="";
+  bookName.value==" ";
+  author.value=="";
+
+
+    
+
+    
+
+    
+
 
 })
